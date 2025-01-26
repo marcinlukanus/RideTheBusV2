fdc753ec947d63f12d85216cef77757e28615fcd
unknown <mlukanus@gmail.com>
2025-01-26 10:17:51 -0600

    we a tailwind shop now boys

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import './App.css';
 import { Game } from './components/Game/Game';
 
 function App() {
   return (
-    <>
-      <h1 className='header'>Ride The Bus</h1>
-      <h4 className='tagline'>The best single-player drinking game around</h4>
+    <div>
+      <h1 className='text-4xl md:text-5xl font-bold leading-tight mb-2'>
+        Ride The Bus
+      </h1>
+      <h4 className='text-xl md:text-2xl italic mt-0 mb-6'>
+        The best single-player drinking game around
+      </h4>
       <Game />
-    </>
+    </div>
   );
 }
 
