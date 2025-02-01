347b48e9faf260fe6dfcc31e2e60b6408e1ac1b3
Marcin Lukanus <mlukanus@gmail.com>
2025-02-01 10:08:42 -0600

    Footy!

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,37 @@
+import { Link } from 'react-router';
+
+export const Footer = (): JSX.Element => {
+  return (
+    <footer className='flex justify-center items-center h-16 bg-black text-white'>
+      <span>Created by Marcin Lukanus</span>
+
+      <span className='mx-2'>|</span>
+
+      <Link to='/'>Home</Link>
+
+      <span className='mx-2'>|</span>
+
+      <Link to='/stats'>Stats</Link>
+
+      <span className='mx-2'>|</span>
+
+      <a
+        href='https://github.com/marcinlukanus/RideTheBusV2'
+        target='_blank'
+        rel='noreferrer'
+      >
+        GitHub
+      </a>
+
+      <span className='mx-2'>|</span>
+
+      <a
+        href='https://buymeacoffee.com/ridethebus'
+        target='_blank'
+        rel='noreferrer'
+      >
+        Buy me a beer!
+      </a>
+    </footer>
+  );
+};
