8c908d34f83f48ad182fc305635ed3974002e88f
Marcin Lukanus <mlukanus@gmail.com>
2026-02-26 23:02:22 -0600

    Revert "Fix Netlify deployment for TanStack Start SSR"
    This reverts commit fd514f8b9d2c07cc08a661ebebeae46f64e29a12.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
-import netlify from '@netlify/vite-plugin-tanstack-start';
 import tsConfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
@@ -11,7 +10,6 @@ export default defineConfig({
     tailwindcss(),
     tsConfigPaths({ projects: ['./tsconfig.json'] }),
     tanstackStart({ srcDirectory: 'src' }),
-    netlify(),
     react(),
   ],
 });
